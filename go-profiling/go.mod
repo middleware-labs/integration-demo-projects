35131f5a92802331f62b20588010248f61f634b0
arjun-mw <arjun.prajapati@middleware.io>
2024-12-13 12:51:21 +0530

    added compose files

---
+++ go-profiling/go.mod
@@ -0,0 +1,47 @@
+module go-profiling
+
+go 1.22
+
+require (
+	github.com/middleware-labs/golang-apm v1.3.0
+	github.com/middleware-labs/golang-apm-http v0.0.2
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fluent/fluent-logger-golang v1.9.0 // indirect
+	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/grafana/pyroscope-go v1.1.0 // indirect
+	github.com/grafana/pyroscope-go/godeltaprof v0.1.6 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
+	github.com/klauspost/compress v1.17.3 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/tinylib/msgp v1.1.9 // indirect
+	go.opentelemetry.io/contrib/bridges/otellogrus v0.2.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.47.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.51.0 // indirect
+	go.opentelemetry.io/contrib/propagators/b3 v1.22.0 // indirect
+	go.opentelemetry.io/otel v1.27.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.3.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.45.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0 // indirect
+	go.opentelemetry.io/otel/log v0.3.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.27.0 // indirect
+	go.opentelemetry.io/otel/sdk/log v0.3.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240520151616-dc85e6b867a5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
---
+++ go-profiling/profiling.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	mwhttp "github.com/middleware-labs/golang-apm-http/http"
+	otellog "github.com/middleware-labs/golang-apm/mwotellogrus"
+	track "github.com/middleware-labs/golang-apm/tracker"
+	log "github.com/sirupsen/logrus"
+)
+
+func StandardFields(ctx context.Context) log.Fields {
+	return log.Fields{
+		// "trace_id": track.TraceID(ctx),
+		// "tid":      track.TraceID(ctx),
+		// "sid":      track.SpanID(ctx),
+		// "span_id":  track.SpanID(ctx),
+		// "tes_attr":        "testing",
+		otellog.MWTraceID: track.TraceID(ctx),
+		otellog.MWSpanID:  track.SpanID(ctx),
+	}
+}
+
+func main() {
+
+	config, _ := track.Track(
+		track.WithConfigTag(track.Service, "golang-logrus-service"),
+		track.WithConfigTag(track.Project, "golang-logrus-service"),
+		// track.WithConfigTag(track.Token, "zuxpjjypnejbbwhkvkobudfitutobptgonae"), // kgcgk
+		// track.WithConfigTag(track.Token, "kqepimwfqqkqhszrqkvlbvevskojjbznbjml"), // nlruf
+		// track.WithConfigTag(track.Token, "myimuvbvhfzvevleucdxhewjqmyiqzhckgol"), //cwext
+		track.WithConfigTag(track.Token, "lneevcqdcnzccpdpbhelkxpjffdxeznkryvb"), //cwext
+	)
+	// lneevcqdcnzccpdpbhelkxpjffdxeznkryvb
+	logHook := otellog.NewMWOTelHook(config, otellog.WithLevels(log.AllLevels), otellog.WithName("otellogs"))
+
+	// add hook in logrus
+	log.AddHook(logHook)
+	// set formatter
+	log.SetFormatter(&log.JSONFormatter{})
+
+	//use mwhttp for http handler instrumentation
+	http.Handle("/hello", mwhttp.MiddlewareHandler(http.HandlerFunc(helloHandler), "hello"))
+	fmt.Println("listening on 8090")
+
+	// this make continuos requests
+	go makeRequest()
+
+	// start the server
+	http.ListenAndServe(":8090", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	time.Sleep(2 * time.Second)
+	//set context in logrus for correlation
+	logger := log.WithFields(StandardFields(r.Context()))
+
+	logger.Info("GO12345678 Info helloHandler testing 123456")
+	logger.Debug("GO12345678 Debug helloHandler testing 123456")
+	logger.Error("GO12345678 Error helloHandler testing 123456")
+
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func makeRequest() {
+	for {
+		time.Sleep(1 * time.Second)
+		http.Get("http://localhost:8090/hello")
+	}
+}
